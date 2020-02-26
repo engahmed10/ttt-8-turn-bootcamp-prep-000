12286b1a854f30f98e9381b6098c1bd24292eeec
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-26 18:24:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,8 +5,17 @@ def display_board(board)
   puts(" #{board[3]} | #{board[4]} | #{board[5]} ")
   puts("-----------")
   puts(" #{board[6]} | #{board[7]} | #{board[8]} ")
-
 end
+
 def turn(board)
   puts "Please enter 1-9:"
+  input = gets.strip
+end
+
+def input_to_index(user_input)  
+   user_input.to_i
+   user_input-1
+   if user_input =="invalid"
+     -1
+   end
 end
