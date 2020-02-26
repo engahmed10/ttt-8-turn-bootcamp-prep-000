6df0629cac6d5ac2175994b2024e9ac7369ca652
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-26 18:41:15 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,12 +11,42 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index=input_to_index(input)
-
-  if index 
+  if index == valid_move?
+     
+     board[index] =  input
+  else
+    
+  end
+  
 end
 
 def input_to_index(user_input)
    userinput=user_input.to_i
    userinput= userinput-1
 
+end
+
+# code your #valid_move? method here
+def valid_move?(board, index)
+  if position_taken?(board, index)
+      false
+    elsif index > board.length() || index < 0
+     false
+    else
+     true
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+# code your #position_taken? method here!
+def position_taken? (board, index)
+   if board[index] == " "
+     false
+   elsif board[index] == ""
+     false
+   elsif board[index] == nil
+     false
+   elsif board[index] == "X" || board[index] == "O"
+    true
+   end
 end
